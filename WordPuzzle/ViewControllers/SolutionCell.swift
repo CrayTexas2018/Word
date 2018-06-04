54a5f9b2b5ae90230c6150ca08a0f8396a70345a
Cray Jaeger <cray@alternascript.com>
2018-06-04 03:41:02 -0500

    puzzle state

---
+++ WordPuzzle/ViewControllers/SolutionCell.swift
@@ -0,0 +1,24 @@
+//
+//  SolutionCell.swift
+//  WordPuzzle
+//
+//  Created by Cray Jaeger on 6/4/18.
+//  Copyright © 2018 Cray Jaeger. All rights reserved.
+//
+
+import UIKit
+
+class SolutionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
